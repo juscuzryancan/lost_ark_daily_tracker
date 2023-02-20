7c146acad45385b030c8c03eef2f53c0b3e47caf
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-02-19 20:20:49 -0600

    completes first rendering test

--- src/__tests__/CharacterSelect.test.tsx
+++ src/__tests__/CharacterSelect.test.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { CharacterSelect } from "../components";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("Character Select", () => {
-  it("Renders a list of characters", () => {
-    const { debug } = render(<CharacterSelect />);
-    debug();
+  it("Renders", () => {
+    render(<CharacterSelect />);
+    expect(screen.queryByTestId("character-select")).toBeInTheDocument();
   });
+  
+
 });
--- src/components/CharacterSelect.tsx
+++ src/components/CharacterSelect.tsx
@@ -40,7 +40,7 @@ const CharacterTab = ({
 }
 
 const CharacterSelect = () => {
-  const [characters, setCharacters] = useState(["Kageyama"]);
+  const [characters, setCharacters] = useState<Array<string>>([]);
 
   const handleAddCharacter = () => {
     setCharacters([...characters, "New Character"])
@@ -49,6 +49,7 @@ const CharacterSelect = () => {
   return (
     <nav
       className="flex"
+      data-testid="character-select"
     >
       {characters.map((character, i) => {
         return (
